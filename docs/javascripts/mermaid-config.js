9f757de2be187c4079e5857d1e960f2721823ddf
Paolo Veronelli <paolo.veronelli@gmail.com>
2026-04-02 08:33:29 +0100

    fix: intercept mermaidConfig via defineProperty to inject securityLevel

--- docs/javascripts/mermaid-config.js
+++ docs/javascripts/mermaid-config.js
@@ -1,7 +1,14 @@
-// Override mermaid config to allow HTML labels in nodes
-window.mermaidConfig = {
-  default: {
-    startOnLoad: false,
-    securityLevel: "loose"
-  }
-};
+// Patch mermaid config after mermaid2 plugin sets it
+// mermaid2 overwrites window.mermaidConfig, so we intercept via defineProperty
+Object.defineProperty(window, "mermaidConfig", {
+  set(val) {
+    if (val && val.default) {
+      val.default.securityLevel = "loose";
+    }
+    this._mermaidConfig = val;
+  },
+  get() {
+    return this._mermaidConfig;
+  },
+  configurable: true
+});
